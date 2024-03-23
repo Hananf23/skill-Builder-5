eb467badae16977711619303161a07e6497e7341
hfarooq23 <hananfarooq2005@gmail.com>
2024-03-22 23:18:46 -0400

    gg

--- src/main/java/SkillBuilder5.java
+++ src/main/java/SkillBuilder5.java
@@ -1,73 +1,98 @@
-/**
- * This class provides various Skill Builder 5 - Array
- * exercises.
- *
- * @author (you)
- * @version (a version number or a date)
- */
-public class SkillBuilder5
-{
-    /**
-     * Calculates the prefix average of array data and returns the prefex average
-     * in a new array.  The parameter data is never touched and left intact.
-     *
-     * @param data array of double values on which to calculate the prefix average.
-     * @return returns an array containing the prefix average values calculated
-     *         from data.
-     */
-    public static double[] prefixAverage(double[] data)
-    {
-        //TODO: replace this line with your code.
-        return null;
+public class SkillBuilder5 {
+
+    public static double[] prefixAverage(double[] data) {
+        int n = data.length;
+        double[] prefixAverages = new double[n];
+        double prefixSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefixSum += data[i];
+            prefixAverages[i] = prefixSum / (i + 1);
+        }
+
+        return prefixAverages;
     }
 
-    /**
-     * Finds the location in array anArray where value is located.  If anArray
-     * does not contain an element equal to value a -1 is returned; otherwise a
-     * positive or zero index value is returned.
-     * @param searchValue value to look for in the array
-     * @param anArray array in which to look for a value
-     * @returns index of the value in the array; -1 otherwise.
-     */
-    public static int  indexOf(int searchValue, int[] anArray)
-    {
-        //TODO: replace this line with your code.
-        return 0;
+    public static int indexOf(int searchValue, int[] anArray) {
+        for (int i = 0; i < anArray.length; i++) {
+            if (anArray[i] == searchValue) {
+                return i;
+            }
+        }
+        return -1; // If the value is not found
     }
 
-    /**
-     * Finds the location in array anArray where string s is located.  If anArray
-     * does not contain an element equal to s a -1 is returned; otherwise a
-     * positive or zero index value is returned.
-     * @param s a string to look for in the array
-     * @param anArray array in which to look for a value
-     * @returns index of the string s in the array; -1 otherwise.
-     */
-    public static int  indexOf(String s, String[] anArray)
-    {
-        //TODO: replace this line with your code.
-        return 0;
+    public static int indexOf(String s, String[] anArray) {
+        for (int i = 0; i < anArray.length; i++) {
+            if (anArray[i].equals(s)) {
+                return i;
+            }
+        }
+        return -1; // If the string is not found
     }
 
-    /**
-     * Finds all occurrence of string s in anArray and removes them, returning
-     * a new array with all occurrences of s removed.
-     * @param s string to search for in array
-     * @param anArray array in which to search and remove s
-     * @return An array with all occurrences of s removed.
-     */
-    public static String[] remove(String s, String[] anArray)
-    {
-        // add your code here
-        return null;
+    public static String[] remove(String s, String[] anArray) {
+        int count = 0;
+
+        for (String str : anArray) {
+            if (str.equals(s)) {
+                count++;
+            }
+        }
+
+        String[] newArray = new String[anArray.length - count];
+        int index = 0;
+
+        for (String str : anArray) {
+            if (!str.equals(s)) {
+                newArray[index++] = str;
+            }
+        }
+
+        return newArray;
     }
 
-    /**
-     * Reverses an array of integers.
-     * @param anArray the array whose contents should be reversed.
-     */
-    public static void reverse(int[] anArray)
-    {
-        // add your code here
+    public static void reverse(int[] anArray) {
+        int start = 0;
+        int end = anArray.length - 1;
+
+        while (start < end) {
+            int temp = anArray[start];
+            anArray[start] = anArray[end];
+            anArray[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        double[] myDatum = {2, 4, 6, 8, 10};
+        double[] pa = prefixAverage(myDatum);
+        System.out.print("pa\n*----> ");
+        for (double avg : pa) {
+            System.out.print(avg + " ");
+        }
+
+        int loc1 = indexOf(5, new int[]{8, 99, 45, 5, 34, 87, 5, 22, 1});
+        System.out.println("\nIndex of 5: " + loc1);
+
+        int loc2 = indexOf("python", new String[]{"za", "hello", "bye", "anaconda", "python", "pycharm", "python"});
+        System.out.println("Index of 'python': " + loc2);
+
+        String[] myArray = {"French", "English", "Spanish", "Greek", "Russian", "Spanish"};
+        myArray = remove("Spanish", myArray);
+        System.out.print("myArray\n*----> ");
+        for (String str : myArray) {
+            System.out.print("\"" + str + "\"");
+        }
+
+        int[] na = {1, 2, 3, 4, 5, 6};
+        reverse(na);
+        System.out.print("\nna\n*----> ");
+        for (int num : na) {
+            System.out.print(num);
+        }
     }
 }
